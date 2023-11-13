cec9d5d57c52b678e731c93979fe1d5dd10e3460
Daniel Watchman <dannietortee@gmail.com>
2023-11-12 16:45:43 -0800

    The printf

--- _printf.c
+++ _printf.c
@@ -10,51 +10,44 @@
  */
 int _printf(const char *format, ...)
 {
-    if (format == NULL)
-        return -1;
-
-    int count = 0;
-    va_list args;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1) != '\0')
-        {
-            format++; /* Move to the character after '%' */
-
-            switch (*format)
-            {
-            case 'c':
-                count += write(1, &va_arg(args, int), 1);
-                break;
-            case 's':
-                count += write(1, va_arg(args, char *), 1);
-                break;
-            case '%':
-                count += write(1, "%", 1);
-                break;
-            case 'd':
-            case 'i':
-                count += write_integer(va_arg(args, int));
-                break;
-            default:
-                count += write(1, "%", 1);
-                count += write(1, format, 1);
-            }
-        }
-        else
-        {
-            count += write(1, format, 1);
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+if (format == NULL)
+return (-1);
+int count = 0;
+va_list args;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%' && *(format + 1) != '\0')
+{
+format++;
+switch (*format)
+{
+case 'c':
+count += write(1, &va_arg(args, int), 1);
+break;
+case 's':
+count += write(1, va_arg(args, char *), 1);
+break;
+case '%':
+count += write(1, "%", 1);
+break;
+case 'd':
+case 'i':
+count += write_integer(va_arg(args, int));
+break;
+default:
+count += write(1, "%", 1);
+count += write(1, format, 1);
+}
+}
+else
+{
+count += write(1, format, 1);
+}
+format++;
+}
+va_end(args);
+return (count);
 }
 
 /**
@@ -65,11 +58,11 @@ int _printf(const char *format, ...)
  */
 int write_integer(int n)
 {
-    char buffer[20]; /* Assuming a 32-bit integer */
-    int count;
+char buffer[20];
+int count;
 
-    count = sprintf(buffer, "%d", n);
-    write(1, buffer, count);
+count = sprintf(buffer, "%d", n);
+write(1, buffer, count);
 
-    return count;
+return (count);
 }
