6c48178c3ee57d874910d935e5f429f474c70d6e
Daniel Watchman <dannietortee@gmail.com>
2023-11-14 02:20:14 -0800

    Add the main guy

--- main.h
+++ main.h
@@ -6,31 +6,33 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int *printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR(va_list arg);
-
 /**
  * struct identifierStruct - structure definition of a printTypeStruct
  * @indentifier: type
  * @printer: function to print
  */
-typedef struct identifierStruct
+typedef struct idStruct
 {
 char *indentifier;
 int (*printer)(va_list);
-} identifierStruct;
+} idStruct;
+
+
+int _putchar(char c);
+int _printInt(va_list arg);
+int _printUnsigned(va_list arg);
+int _printf(const char *format, ...);
+int _printChar(va_list arg);
+int _printStr(va_list arg);
+int _printPercent(void);
+void _printBinary(unsigned int n, unsigned int *printed);
+int _printUnsignedToBinary(va_list arg);
+int _printOct(va_list arg);
+int _printUnsignedIntToHex(unsigned int num, char _case);
+int _printHexBase(va_list arg, char _case);
+int _printHex(va_list arg);
+int _printHEX(va_list arg);
+int _printSTR(va_list arg);
+
 
 #endif
