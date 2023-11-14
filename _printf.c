56c7ea44b2a59a4db6aacb14e48aff201513b3e7
Daniel Watchman <dannietortee@gmail.com>
2023-11-14 02:39:24 -0800

    The Printer of Formats

--- _printf.c
+++ _printf.c
@@ -4,38 +4,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * printIdentifiers - prints special characters
- * @next: character after the %
+ * printIds - prints special characters
+ * @after: character after the %
  * @arg: argument for the indentifier
  * Return: the number of characters printed
  * (excluding the null byte used to end output to strings)
  */
-int printIdentifiers(char next, va_list arg)
+int printIds(char after, va_list arg)
 {
-	int functsIndex;
-	identifierStruct functs[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"u", print_unsigned},
-		{"b", print_unsignedToBinary},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
+	int funsIndex;
+	idStruct funs[] = {
+		{"c", _printChar},
+		{"s", _printStr},
+		{"d", _printInt},
+		{"i", _printInt},
+		{"u", _printUnsigned},
+		{"b", _printUnsignedToBinary},
+		{"o", _printOct},
+		{"x", _printHex},
+		{"X", _printHEX},
+		{"S", _printSTR},
 		{NULL, NULL}
 	};
 
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+	for (funsIndex = 0; funs[funsIndex].indentifier != NULL; funsIndex++)
 	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
+		if (funs[funsIndex].indentifier[0] == after)
+			return (funs[funsIndex].printer(arg));
 	}
 	return (0);
 }
 /**
- * _printf - mimic printf from stdio
+ * _printf - Print out the contents
  * Description: produces output according to a format
  * write output to stdout, the standard output stream
  * @format: character string composed of zero or more directives
@@ -47,7 +47,7 @@ int printIdentifiers(char next, va_list arg)
 int _printf(const char *format, ...)
 {
 	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
+	int identifierPrinted = 0, charWritten = 0;
 	va_list arg;
 
 	va_start(arg, format);
@@ -59,31 +59,31 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			charPrinted++;
+			charWritten++;
 			continue;
 		}
 		if (format[i + 1] == '%')
 		{
 			_putchar('%');
-			charPrinted++;
+			charWritten++;
 			i++;
 			continue;
 		}
 		if (format[i + 1] == '\0')
 			return (-1);
 
-		identifierPrinted = printIdentifiers(format[i + 1], arg);
+		identifierPrinted = printIds(format[i + 1], arg);
 		if (identifierPrinted == -1 || identifierPrinted != 0)
 			i++;
 		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
+			charWritten += identifierPrinted;
 
 		if (identifierPrinted == 0)
 		{
 			_putchar('%');
-			charPrinted++;
+			charWritten++;
 		}
 	}
 	va_end(arg);
-	return (charPrinted);
+	return (charWritten);
 }
