a3c40775e8b09f122160c23b865efd96e62d5931
Daniel Watchman <dannietortee@gmail.com>
2023-11-14 02:22:25 -0800

    Lets have fun. LOL

--- funs.c
+++ funs.c
@@ -2,56 +2,56 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * print_char - writes the character c to stdout
+ * _printChar - writes the character c to stdout
  * @arg: argument
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int print_char(va_list arg)
+int _printChar(va_list arg)
 {
 	return (_putchar(va_arg(arg, int)));
 }
 
 /**
- * print_int - prints an integer.
+ * _printInt - prints an integer.
  * @arg: argument
  * Return: 0
  */
 
-int print_int(va_list arg)
+int _printInt(va_list arg)
 {
 
-unsigned int divisor = 1, i, resp, charPrinted = 0;
+unsigned int div = 1, i, res, charWritten = 0;
 int n = va_arg(arg, int);
 
 if (n < 0)
 {
 	_putchar('-');
-	charPrinted++;
+	charWritten++;
 	n *= -1;
 }
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
+for (i = 0; n / div > 9; i++, div *= 10)
 ;
 
-for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+for (; div >= 1; n %= div, div /= 10, charWritten++)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+	res = n / div;
+	_putchar('0' + res);
 }
-return (charPrinted);
+return (charWritten);
 }
 
 
 
 /**
- * print_STR - prints a string with a `S` (upper case) specificer
+ * _printSTR - prints a string with a `S` (upper case) specificer
  * @arg: argument
  * Return: number of character printed
  */
 
-int print_STR(va_list arg)
+int _printSTR(va_list arg)
 {
 int i;
 char *str = va_arg(arg, char*);
@@ -70,7 +70,7 @@ for (i = 0; str[i]; i++)
 		if (i < 16)
 			_putchar('0');
 
-		print_unsignedIntToHex(str[i], 'A');
+		_printUnsignedIntToHex(str[i], 'A');
 	}
 	else
 		_putchar(str[i]);
@@ -80,12 +80,12 @@ return (i);
 }
 
 /**
- * print_str - prints a string with a `s` (lower case) specifier
+ * _printStr - prints a string with a `s` (lower case) specifier
  * @arg: argument
  * Return: number of character printed
  */
 
-int print_str(va_list arg)
+int _printStr(va_list arg)
 {
 int i;
 char *str = va_arg(arg, char*);
@@ -102,23 +102,23 @@ return (i);
 }
 
 /**
- * print_unsigned - prints an unsigned int.
+ * _printUnsigned - prints an unsigned int.
  * @arg: argument
  * Return: 0
  */
 
-int print_unsigned(va_list arg)
+int _printUnsigned(va_list arg)
 {
-int divisor = 1, i, resp;
+int div = 1, i, res;
 unsigned int n = va_arg(arg, unsigned int);
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
+for (i = 0; n / div > 9; i++, div *= 10)
 ;
 
-for (; divisor >= 1; n %= divisor, divisor /= 10)
+for (; div >= 1; n %= div, div /= 10)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+	res = n / div;
+	_putchar('0' + res);
 }
 return (i + 1);
 }
